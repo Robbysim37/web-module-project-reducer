bdf52c01e952e2b238181f4e732f9f248f064f73
Robby Lewis <Robbysim37@gmail.com>
2022-12-14 16:19:54 -0500

    finished assignment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { 
+    ADD_ONE,
+    APPLY_NUMBER,
+    CHANGE_OPERATION,
+    CLEAR_DISPLAY,
+    CLEAR_MEMORY,
+    SET_MEMORY
+} from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -37,6 +44,24 @@ const reducer = (state, action) => {
                 operation: action.payload
             });
             
+        case(CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
+            })
+        
+        case(SET_MEMORY):
+            return({
+                ...state,
+                memory: state.total
+            })
+
+        case(CLEAR_MEMORY):
+            return({
+                ...state,
+                memory: 0
+            })
+
         default:
             return state;
     }
